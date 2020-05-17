75fffe7dc999c73a381ace13a02489d6a47fbc8e
dapingtai <zero102x@gmail.com>
2020-05-17 20:03:37 +0800

    Ver0.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='Tbqc',
-      version='0.0.1',
+      version='0.0.3',
       description='Taiwan Bartender Quizdata Crawler',
       url='https://github.com/dapingtai/Tw-Bartender-Quizdata-crawler/',
       author='HungChang',
